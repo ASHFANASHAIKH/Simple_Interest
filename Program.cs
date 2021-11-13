5a728bfd4eea400a31b601dcfc66032413d1447c
ashfana <ashfana@DESKTOP-NPR6MTA>
2021-11-13 12:45:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Interest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            double princamt, rate, interest, total_amt;
+            Console.Write("Enter The Loan Amount: ");
+            princamt = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the no. of years: ");
+            year = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter the rate of interest: ");
+            rate = Convert.ToDouble(Console.ReadLine());
+            interest = princamt * year * rate / 100;
+            total_amt = princamt + interest;
+            Console.WriteLine("total Amount: {0}", total_amt);
+            Console.ReadLine();
+        }
+    }
+}
